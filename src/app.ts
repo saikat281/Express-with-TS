1354b2ec7ae10bd53fd295d720b046578cbb3d8b
Saikat Sarkar <sarkarsaikat3467@gmail.com>
2026-05-23 17:48:22 +0600

    Create auth middleware

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,8 @@ import { pool } from "./db"
 import { userRoute } from "./modules/user/user.route"
 import { profileRoute } from "./modules/profile/profile.route"
 import { authRoute } from "./modules/auth/auth.route"
+import fs from "fs"
+import logger from "./middleware/logger"
 const app: Application = express()
 
 
@@ -10,11 +12,26 @@ app.use(express.json())
 app.use(express.text())
 app.use(express.urlencoded({ extended: true }))
 
+app.use(logger)
 
-app.use('/api/users',userRoute)
+app.get('/', async (req: Request, res: Response) => {
+    try {
+        res.status(201).json({
+            message: "Express server",
 
-app.use('/api/profileS',profileRoute)
-app.use('/api/auth',authRoute)
+        })
+    } catch (error:any) {
+        res.status(500).json({
+            message: error.message,
+            error: error
+        })
+    }
+})
+
+app.use('/api/users', userRoute)
+
+app.use('/api/profileS', profileRoute)
+app.use('/api/auth', authRoute)
 
 
 
